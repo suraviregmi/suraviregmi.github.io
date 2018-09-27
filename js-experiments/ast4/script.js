25a185bb2c497d731d9c4410daac4963e120ff24
suraviregmi <regmisuravi@gmail.com>
2018-09-27 11:34:00 +0545

    sliding both sides , left and right button also work.

--- js-experiments/ast4/script.js
+++ js-experiments/ast4/script.js
@@ -1,11 +1,19 @@
-var wrapper = document.getElementsByClassName('viewport');
+var wrapper = document.getElementsByClassName('viewport')[0];
 var imageSlider = document.getElementsByClassName('images')[0];
-var noOfChilldren = imageSlider.getElementsByTagName('img');
-//console.log(noOfChilldren.length)
+var Children = imageSlider.getElementsByTagName('img');
+var leftButton = document.getElementById("leftBtn");
+var rightButton = document.getElementById("rightBtn");
+
+noOfChildren = Children.length
 var width = 768;
-var status = 1;
-var index = 0;
-var startLoop, startleftloop;
+var currentMarginLeft = 0;
+var currentIndex = 0;
+var clickToSlide = 0;
+var refStartLoop, refAnimate;
+var shifted = 0;
+var nextDirection = -1;
+var animationTime;
+var animationRunning = 1;
 
 
 //to run on startup
@@ -13,29 +21,81 @@ window.onload = function() {
     startSlide();
 }
 
-//to loop slider in interval 5 sec
-startLoop = setInterval(function() {
-    startSlide();
-}, 2000);
 
 
-//function to slide right
 function startSlide() {
-    var imgMarginLeft = -index * width;
-    imageSlider.style.marginLeft = imgMarginLeft + "px";
-    index = (index + 1) % noOfChilldren.length;
+
+    refStartSlide = setInterval(function() {
+        if (currentIndex === 0) {
+            nextDirection = -1;
+            increment = 1;
+        } else if (currentIndex == noOfChildren - 1) {
+            nextDirection = 1;
+            increment = -1;
+        }
+        currentIndex = currentIndex + increment;
+        // console.log("current indx add", currentIndex);
+        //console.log("Next Value is ", nextDirection);
+
+        animate(nextDirection);
+
+
+    }, 5000);
 }
 
 
-document.getElementById("leftBtn").onclick = function() {
+function animate(nextDirection) {
+    animationRunning = 1;
+    stopSlide();
+    //console.log("slideStopped and animate")
+    animationTime = 0;
+    refAnimate = setInterval(function() {
+        shifted = shifted + (nextDirection * 768) / 200;
+        if (animationTime === 200) {
+            stopAnimate();
+            // console.log("stop animate")
+        } else {
+            imageSlider.style.marginLeft = shifted + "px";
+        }
+        animationTime++;
+    }, 1);
     startSlide();
-}
-document.getElementById("rightBtn").onclick = function() {
-    startSlide();
+    // console.log("start slide again");
 }
 
 
-//to stop the interval
+// only change when not animating and dont go further than last image
+rightButton.onclick = function() {
+
+    if ((currentIndex < noOfChildren - 1) && (animationRunning === 0)) {
+        currentIndex++;
+        animate(-1)
+    }
+}
+// only change when not animating and dont go further than first image
+leftButton.onclick = function() {
+
+    if ((currentIndex > 0) && (animationRunning === 0)) {
+        currentIndex--;
+        animate(1)
+    }
+}
+
+
+//stop only slide
+function stopSlide() {
+    clearInterval(refStartSlide);
+}
+
+///stop ony animate
+
+function stopAnimate() {
+    clearInterval(refAnimate);
+    animationRunning = 0;
+}
+
+//stop animate and slide
 function stop() {
-    clearInterval(startLoop)
+    stopSlide();
+    stopAnimate();
 }
