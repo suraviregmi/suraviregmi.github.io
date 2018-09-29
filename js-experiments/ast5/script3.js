c5c487b713f671459a310864b87701533d2a5fde
suraviregmi <regmisuravi@gmail.com>
2018-09-29 18:32:27 +0545

    changes made to ant smasher

--- js-experiments/ast5/script3.js
+++ js-experiments/ast5/script3.js
@@ -2,16 +2,15 @@
 var wrapper = document.createElement('div');
 wrapper.style.width = '700px';
 wrapper.style.height = '500px';
-wrapper.style.background = 'salmon';
+wrapper.style.background = 'yellow';
 wrapper.style.position = 'relative';
 
 document.body.appendChild(wrapper);
 
 var wrapperWidth = removePx(wrapper.style.width);
 var wrapperHeight = removePx(wrapper.style.height);
-
-var dx = 2;
-var dy = 2;
+/*var dx = 2;
+var dy = 2;*/
 
 var antObjectIndex = 0;
 var antArray = [];
@@ -25,55 +24,149 @@ function removePx(stringValue) {
 function Ants(antId) {
     this.x = Math.floor(Math.random() * wrapperWidth) + 1;
     this.y = Math.floor(Math.random() * wrapperHeight) + 1;
-    this.antWidth = 30;
-    this.antHeight = 30;
-    this.antColor = getRandomColor();
+    this.antWidth = 5;
+    this.antHeight = 5;
+    this.antColor = 'black';
     //small random difference
-    this.dx = Math.floor(Math.random() * 5);
-    this.dy = Math.floor(Math.random() * 5);
+    this.dx = Math.floor(Math.random() * 2) + 1;
+    this.dy = Math.floor(Math.random() * 2) + 1;
+    this.isAlive = true;
+    this.el;
+    var that = this;
 
     this.createAnt = function() {
         var newAnt = document.createElement('div');
+        this.el = newAnt;
         newAnt.style.left = this.x + 'px';
         newAnt.style.top = this.y + 'px';
         newAnt.style.width = this.antWidth + 'px';
         newAnt.style.height = this.antHeight + 'px';
         newAnt.style.background = this.antColor;
-        newAnt.style.borderRadius = '50%';
+        newAnt.style.borderRadius = '0';
         newAnt.style.position = 'absolute';
+        //console.log("beofre click")
+        //console.log(newAnt)
         wrapper.appendChild(newAnt);
+        newAnt.onclick = function() {
+            //console.log("cclicked");
+            wrapper.removeChild(that.el);
+        }
+
+
     }
+
+
 
     this.updatePosition = function() {
+
+
         this.x = this.x + this.dx;
         this.y = this.y + this.dy;
-        if (this.x >= wrapperWidth - this.antWidth) {
+
+        this.el.style.top = this.y + 'px';
+        this.el.style.left = this.x + 'px';
+
+
+    }
+    this.handleCollsionWall = function() {
+        //right
+        if (this.x > wrapperWidth - this.antWidth) {
             this.dx = -this.dx;
+            this.x = wrapperWidth - this.antWidth;
         }
+        //left
         if (this.x < this.antWidth) {
             this.dx = -this.dx;
+            this.x = this.antWidth;
+            //this.x = 0;
         }
-
+        //bottom
         if (this.y > wrapperHeight - this.antHeight) {
             this.dy = -this.dy;
+            this.y = wrapperHeight - this.antHeight;
         }
+        //top
         if (this.y < this.antHeight) {
             this.dy = -this.dy;
+            this.y = this.antHeight;
         }
     }
+
+    this.handleAntCollision = function(index) {
+        for (var i = 0; i < antArray.length; i++) {
+            //if nott oneself then handle collisoin
+
+            if (this.isAlive === false) {
+                this.antWidth = 0;
+                this.antHeight = 0;
+
+            }
+            if (i !== index) {
+
+                if (this.x < antArray[i].x + antArray[i].antWidth &&
+                    this.x + this.antWidth > antArray[i].x &&
+                    this.y < antArray[i].y + antArray[i].antHeight &&
+                    this.y + this.antHeight > antArray[i].y) {
+                    //collid
+                    this.dx = -this.dx;
+                    this.dy = -this.dy;
+
+                    if (this.dx < 0) {
+                        this.x += 10;
+                    }
+                    if (this.dx > 0) {
+                        this.x -= 10;
+                    }
+                    if (this.dy < 0) {
+                        this.y += 10;
+                    }
+                    if (this.dy > 0) {
+                        this.x -= 10;
+                    }
+                    /* if (this.dy < 0 || this.dy) {
+     this.dy = -this.dy;
+ }*/
+                    //this.dy = -this.dy;
+                }
+            }
+        }
+    }
+
+
+    /*    this.handleMouseClicks = function(mouseX, mouseY) {
+            // this.xy === x, y comapre if equal then hide
+            if (mouseX > this.x &&
+                mouseX < this.x + this.antWidth &&
+                mouseY > this.y &&
+                mouseY < this.y + this.antHeight) {
+                console.log("clicked");
+
+                this.antHeight = 0;
+                this.antWidth = 0;
+            }
+        }*/
+
+
 }
 
-var getRandomColor = function() {
+
+/*var getRandomColor = function() {
     var letters = '0123456789ABCDEF';
     var color = '#';
     for (var i = 0; i < 6; i++) {
         color += letters[Math.floor(Math.random() * 16)];
     }
     return color;
-}
+}*/
 
+/*function handleclick(event) {
+    for (var i = 0; i < antArray.length; i++) {
+        antArray[i].handleMouseClicks(event.clientX, event.clientY)
+    }
+}
+document.addEventListener("click", handleclick);*/
 function initalScene() {
-    for (i = 0; i < 50; i++) {
+    for (i = 0; i < 1000; i++) {
         //create a new single ant
         var singleAnt = new Ants(i);
         singleAnt.createAnt();
@@ -83,12 +176,16 @@ function initalScene() {
 
 function startMoving() {
     //like in canvas
-    clearWrapper();
-    for (i = 0; i < 50; i++) {
-        antContext = antArray[i];
+    // clearWrapper();
+    for (i = 0; i < 1000; i++) {
+
         //update and move 
-        antContext.updatePosition();
-        antContext.createAnt();
+        // antArray[i].createAnt();
+
+        antArray[i].updatePosition();
+        antArray[i].handleCollsionWall();
+        antArray[i].handleAntCollision(i);
+
     }
 }
 
